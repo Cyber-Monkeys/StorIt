98ec30e11173049e6b016be5c2528a020088796e
fidellim <lim.fidel@gmail.com>
2020-01-14 23:12:26 +0400

    add plans page

---
+++ app/src/main/java/com/example/storit/Plans.java
@@ -0,0 +1,14 @@
+package com.example.storit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plans);
+    }
+}

e6858cf6207d52b01789c4d85b677929bf18eecd
fidellim <lim.fidel@gmail.com>
2020-01-16 02:00:25 +0400

    add toolbar

--- app/src/main/java/com/example/storit/Help.java
+++ app/src/main/java/com/example/storit/Help.java
@@ -1,14 +1,39 @@
 package com.example.storit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class Help extends AppCompatActivity {
+
+    //Variables
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        //Toolbar for this page
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Help");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        final Drawable goBack = getResources().getDrawable(R.drawable.ic_go_back);
+        goBack.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(goBack);
+        //go back button
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }

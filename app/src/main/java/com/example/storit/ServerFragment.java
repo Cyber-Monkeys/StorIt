b4f487b8c834babd84bdc22f279ddc6a9cc8bccd
fidellim <lim.fidel@gmail.com>
2020-01-20 03:06:46 +0400

    update imports

--- app/src/main/java/com/example/storit/ServerFragment.java
+++ app/src/main/java/com/example/storit/ServerFragment.java
@@ -5,12 +5,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.ListFragment;
 
 import java.util.ArrayList;

40e22b94ba60de2909b654a58045eed772424637
fidellim <lim.fidel@gmail.com>
2020-01-20 03:07:43 +0400

    add client adapter for gridview

---
+++ app/src/main/java/com/example/storit/ClientAdapter.java
@@ -0,0 +1,73 @@
+package com.example.storit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class ClientAdapter extends BaseAdapter {
+
+    //variables
+    private Context context;
+    private LayoutInflater inflater;
+    private String[] fileNameArr;
+    private int[] imageArr;
+
+    //constructor
+    public ClientAdapter(Context context, String[] fileName, int[] image){
+        this.context = context;
+        this.fileNameArr = fileName;
+        this.imageArr = image;
+    }
+
+    @Override
+    public int getCount() {
+        return fileNameArr.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null){
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        if (convertView == null){
+            convertView = inflater.inflate(R.layout.client_list, null);
+        }
+
+        ImageView image = convertView.findViewById(R.id.image);
+        ImageView moreOptions = convertView.findViewById(R.id.moreOptions);
+        TextView fileName = convertView.findViewById(R.id.fileName);
+
+        image.setImageResource(imageArr[position]);
+        fileName.setText(fileNameArr[position]);
+
+        //open more options
+        moreOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoreOptionsBottomSheetDialog bottomSheetDialog = new MoreOptionsBottomSheetDialog();
+                bottomSheetDialog.show(((FragmentActivity)context).getSupportFragmentManager(), "AddNewBottomSheet");
+            }
+        });
+
+        return convertView;
+    }
+}

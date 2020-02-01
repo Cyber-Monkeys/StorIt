b2a14f2bcf8e17e8624bf19ec9e27d0c7755bd00
fidellim <lim.fidel@gmail.com>
2020-02-01 19:08:21 +0400

    add space

--- app/src/main/java/com/example/storit/MoreOptionsBottomSheetDialog.java
+++ app/src/main/java/com/example/storit/MoreOptionsBottomSheetDialog.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,4 +20,5 @@ public class MoreOptionsBottomSheetDialog extends BottomSheetDialogFragment {
         View view = inflater.inflate(R.layout.bottom_sheet_more_options, container, false);
         return view;
     }
+
 }

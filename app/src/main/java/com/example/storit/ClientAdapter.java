f5d1e3512321c967722d6b1437caf410e6acfbc6
Humaid Khan <humaidk2@gmail.com>
2020-04-06 22:43:41 +0400

    Integrate file class

--- app/src/main/java/com/example/storit/ClientAdapter.java
+++ app/src/main/java/com/example/storit/ClientAdapter.java
@@ -10,36 +10,38 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import androidx.fragment.app.FragmentActivity;
 
 public class ClientAdapter extends BaseAdapter {
 
     //variables
     private Context context;
-    private LayoutInflater inflater;
-    private String[] fileNameArr;
-    private Integer[] imageArr;
+    private LayoutInflater inflater = null;
+    private ArrayList<File> fileList = new ArrayList<File>();
 
     //constructor
-    public ClientAdapter(Context context, String[] fileName, Integer[] image){
+    public ClientAdapter(Context context, ArrayList<File> fileName){
         this.context = context;
-        this.fileNameArr = fileName;
-        this.imageArr = image;
+        this.fileList = fileName;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return fileNameArr.length;
+        return fileList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return fileList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
@@ -57,8 +59,8 @@ public class ClientAdapter extends BaseAdapter {
         ImageView moreOptions = convertView.findViewById(R.id.moreOptions);
         TextView fileName = convertView.findViewById(R.id.fileName);
 
-        image.setImageResource(imageArr[position]);
-        fileName.setText(fileNameArr[position]);
+        image.setImageResource(fileList.get(position).getFileImage());
+        fileName.setText(fileList.get(position).getFileName());
 
         //open more options
         moreOptions.setOnClickListener(new View.OnClickListener() {
